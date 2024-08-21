620c774ad43482368ddbba1c77408b2bbef23db2
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-21 11:12:15 +0530

    Created the down icon

---
+++ frontend/src/components/Icons/DownIcon.jsx
@@ -0,0 +1,20 @@
+function DownIcon() {
+  return (
+    <svg
+      width="10"
+      height="6"
+      viewBox="0 0 10 6"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.25 1.25L5 5L8.75 1.25"
+        stroke="#475467"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default DownIcon;
