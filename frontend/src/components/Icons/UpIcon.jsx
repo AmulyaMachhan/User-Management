050125e034573ace3f3f8f724e3389c4473ad924
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-21 10:11:14 +0530

    Created the up icon

---
+++ frontend/src/components/Icons/UpIcon.jsx
@@ -0,0 +1,20 @@
+function UpIcon() {
+  return (
+    <svg
+      width="10"
+      height="6"
+      viewBox="0 0 10 6"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.25 4.75L5 1L8.75 4.75"
+        stroke="#475467"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default UpIcon;
