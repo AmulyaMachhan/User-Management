c156869ff28ef8a6f38f1eaaaec3b9847a8eca92
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-22 17:12:17 +0530

    Updated the overview page

--- frontend/src/components/Overview.jsx
+++ frontend/src/components/Overview.jsx
@@ -1,6 +1,6 @@
 function Overview() {
   return (
-    <div className="mx-5 my-2 text-[#101828] text-[36px] font-[600]">
+    <div className="mx-5 my-2 h-[100vh] text-[#101828] text-[36px] font-[600]">
       Welcome John Doe
     </div>
   );
