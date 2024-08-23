195e478d33ca90161d9adb162acecd4bd8580175
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-23 12:45:16 +0530

    Updated the header

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -1,26 +1,21 @@
-import { TbBell } from "react-icons/tb";
+import { BellIcon } from "./Icons/index";
 
 function Header() {
   return (
     <header className="flex justify-between items-center h-[60px] px-4 bg-white shadow-sm">
-      {/* Brand Logo */}
       <div className="text-[#6941C6] text-[24px] font-[800]">PEOPLE.CO</div>
 
-      {/* Right Side Icons */}
       <div className="flex items-center space-x-2">
-        {/* Bell Icon */}
         <div className="p-1">
-          <TbBell size={16} />
+          <BellIcon />
         </div>
 
-        {/* User Avatar */}
         <img
           src="https://images.unsplash.com/photo-1624561172888-ac93c696e10c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjR8fGF2YXRhcnxlbnwwfHwwfHx8MA%3D%3D"
           alt="User Avatar"
           className="w-[28px] h-[28px] rounded-full object-cover"
         />
 
-        {/* Username */}
         <span className="text-gray-700 text-sm font-medium">John Doe</span>
       </div>
     </header>
