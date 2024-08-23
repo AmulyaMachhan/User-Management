79c1371694ab5df37e3c827df4db6cc1fcaac6d3
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-23 10:09:58 +0530

    SearchIcon

---
+++ frontend/src/components/Icons/SearchIcon.jsx
@@ -0,0 +1,21 @@
+function SearchIcon() {
+  return (
+    <svg
+      width="16"
+      height="16"
+      viewBox="0 0 16 16"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M14.75 14.75L10.25 10.25M11.75 6.5C11.75 9.39949 9.39949 11.75 6.5 11.75C3.60051 11.75 1.25 9.39949 1.25 6.5C1.25 3.60051 3.60051 1.25 6.5 1.25C9.39949 1.25 11.75 3.60051 11.75 6.5Z"
+        stroke="#6941C6"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default SearchIcon;
