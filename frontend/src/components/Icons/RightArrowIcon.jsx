5a87e59e876e9b1c3be0181ba720d74abf48e39f
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-23 14:21:45 +0530

    Right Arrow Icon

---
+++ frontend/src/components/Icons/RightArrowIcon.jsx
@@ -0,0 +1,21 @@
+function RightArrowIcon() {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M1.16663 6.99996H12.8333M12.8333 6.99996L6.99996 1.16663M12.8333 6.99996L6.99996 12.8333"
+        stroke="#344054"
+        strokeWidth="1.66667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default RightArrowIcon;
