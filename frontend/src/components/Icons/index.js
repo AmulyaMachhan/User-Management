94121a9760e78b9d91db2535d103ecb791658726
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-23 11:19:45 +0530

    Index for icon export

---
+++ frontend/src/components/Icons/index.js
@@ -0,0 +1,7 @@
+export { default as SearchIcon } from "./SearchIcon";
+export { default as FilterIcon } from "./FilterIcon";
+export { default as DeleteIcon } from "./DeleteIcon";
+export { default as EditIcon } from "./EditIcon";
+export { default as AddIcon } from "./AddIcon";
+export { default as CloseIcon } from "./CloseIcon";
+export { default as BellIcon } from "./BellIcon";
