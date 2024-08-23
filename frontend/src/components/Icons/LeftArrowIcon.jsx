e432610e13727a07ff6584d32fbcdab55284931e
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-23 13:22:11 +0530

    Left Arrow Icon

---
+++ frontend/src/components/Icons/LeftArrowIcon.jsx
@@ -0,0 +1,21 @@
+function LeftArrowIcon() {
+  return (
+    <svg
+      width="14"
+      height="14"
+      viewBox="0 0 14 14"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M12.8334 6.99996H1.16675M1.16675 6.99996L7.00008 12.8333M1.16675 6.99996L7.00008 1.16663"
+        stroke="#344054"
+        strokeWidth="1.66667"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default LeftArrowIcon;
