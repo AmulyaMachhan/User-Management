ee96ea49b2b639ab45b6547e2d1cdee82a7cbcd4
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-24 10:15:26 +0530

    Updated the people Directory component

--- frontend/src/components/PeopleDirectory.jsx
+++ frontend/src/components/PeopleDirectory.jsx
@@ -2,6 +2,9 @@ import { useMemo } from "react";
 import { useSelector } from "react-redux";
 import { createColumnHelper } from "@tanstack/react-table";
 import UserTable from "./Table/UserTable";
+import Name from "./Columns/Name";
+import Status from "./Columns/Status";
+import Teams from "./Columns/Teams";
 
 const PeopleDirectory = () => {
   const users = useSelector((state) => state.user.users);
@@ -10,12 +13,6 @@ const PeopleDirectory = () => {
 
   const data = useMemo(() => users, [users]);
   const columnHelper = createColumnHelper();
-
-  const colorSchemes = [
-    { text: "#6941C6", bg: "#F9F5FF", border: "#E4E7EC" },
-    { text: "#175CD3", bg: "#EFF8FF", border: "#B2DDFF" },
-    { text: "#3538CD", bg: "#EEF4FF", border: "#C7D7FE" },
-  ];
 
   const columns = useMemo(
     () => [
@@ -27,40 +24,15 @@ const PeopleDirectory = () => {
             original: { profileImage, name, username },
           },
         }) => (
-          <div className="flex items-center gap-3">
-            <img
-              src={profileImage}
-              alt={name}
-              className="w-8 h-8 rounded-full"
-            />
-            <div>
-              <div className="text-xs font-medium">{name}</div>
-              <div className="text-xs text-gray-500">@{username}</div>
-            </div>
-          </div>
+          <Name profileImage={profileImage} name={name} username={username} />
         ),
       }),
       columnHelper.accessor("status", {
         id: "status",
         header: "Status",
-        cell: ({ getValue }) => (
-          <div
-            className={`flex items-center justify-center gap-2 text-xs py-1 px-2 border rounded-xl 
-            ${
-              getValue()
-                ? "border-green-300 bg-green-50 text-green-600"
-                : "border-red-300 bg-red-50 text-red-600"
-            }`}
-          >
-            <span
-              className={`h-2 w-2 rounded-full ${
-                getValue() ? "bg-green-500" : "bg-red-500"
-              }`}
-            />
-            {getValue() ? "Active" : "Inactive"}
-          </div>
-        ),
+        cell: ({ getValue }) => <Status isActive={getValue()} />,
       }),
+
       columnHelper.accessor("role", {
         id: "role",
         header: "Role",
@@ -79,38 +51,7 @@ const PeopleDirectory = () => {
         id: "teams",
         header: "Teams",
         cell: ({ getValue }) => {
-          const teams = getValue();
-          const maxVisible = 3;
-          const teamsToShow = teams.slice(0, maxVisible);
-          const extraCount =
-            teams.length > maxVisible ? teams.length - maxVisible : 0;
-
-          return (
-            <div className="flex flex-wrap gap-1 items-center">
-              {teamsToShow.map((team, index) => {
-                const { text, bg, border } =
-                  colorSchemes[index % colorSchemes.length];
-                return (
-                  <div
-                    key={index}
-                    className="text-xs px-2 py-1 rounded-full"
-                    style={{
-                      color: text,
-                      backgroundColor: bg,
-                      borderColor: border,
-                    }}
-                  >
-                    {team}
-                  </div>
-                );
-              })}
-              {extraCount > 0 && (
-                <div className="text-xs px-2 py-1 rounded-full text-gray-600 border border-gray-300">
-                  +{extraCount}
-                </div>
-              )}
-            </div>
-          );
+          return <Teams teams={getValue()} />;
         },
       }),
     ],
