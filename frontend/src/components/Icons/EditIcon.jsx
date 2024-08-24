d5b7c010251c42f6c68ab375ad62c259ed4edf0c
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-24 16:15:12 +0530

    EditIcon

---
+++ frontend/src/components/Icons/EditIcon.jsx
@@ -0,0 +1,21 @@
+function EditIcon() {
+  return (
+    <svg
+      width="15"
+      height="15"
+      viewBox="0 0 20 20"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M15.7083 2.29167C16.1042 1.89583 16.5958 1.83333 16.9167 2.15417C17.25 2.475 17.25 3.01667 16.9167 3.375L7.08333 13.2083L3.33333 13.8333L4.00000 10.0833L13.8333 1.25000C14.2083 0.916667 14.75 0.916667 15.0833 1.25000L15.7083 2.29167Z"
+        stroke="black"
+        strokeWidth="1.5"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default EditIcon;
