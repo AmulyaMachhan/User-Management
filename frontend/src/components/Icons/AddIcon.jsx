06f7cfafb4ab0fbb4a8470e91833290076196277
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-24 13:25:12 +0530

    Created Add Icon

---
+++ frontend/src/components/Icons/AddIcon.jsx
@@ -0,0 +1,18 @@
+function AddIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.16663 6.83329H0.166626V5.16663H5.16663V0.166626H6.83329V5.16663H11.8333V6.83329H6.83329V11.8333H5.16663V6.83329Z"
+        fill="white"
+      />
+    </svg>
+  );
+}
+
+export default AddIcon;
