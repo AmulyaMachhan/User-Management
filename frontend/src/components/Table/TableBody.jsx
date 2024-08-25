782ef67a146e74e8e1001c141ba3ed4aa8c52ce5
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-25 11:17:21 +0530

    Created table body

---
+++ frontend/src/components/Table/TableBody.jsx
@@ -0,0 +1,80 @@
+/* eslint-disable react/prop-types */
+import { flexRender } from "@tanstack/react-table";
+import { DeleteIcon, EditIcon } from "../Icons";
+
+const TableBody = ({
+  table,
+  rows,
+  handleRowClick,
+  setModals,
+  setSelectedUser,
+}) => {
+  const handleEdit = (user) => {
+    setSelectedUser(user);
+    setModals((prev) => ({ ...prev, isModalOpen: true }));
+  };
+
+  const handleDelete = (user) => {
+    setSelectedUser(user);
+    setModals((prev) => ({ ...prev, isDeleteModalOpen: true }));
+  };
+
+  return (
+    <table className="w-full">
+      <thead className="text-[#475467]">
+        {table.getHeaderGroups().map((headerGroup) => (
+          <tr key={headerGroup.id}>
+            {headerGroup.headers.map((header) => (
+              <th
+                key={header.id}
+                className="py-3 px-3 text-left text-sm border-b"
+              >
+                {flexRender(
+                  header.column.columnDef.header,
+                  header.getContext()
+                )}
+              </th>
+            ))}
+          </tr>
+        ))}
+      </thead>
+      <tbody>
+        {rows.map((row) => (
+          <tr
+            key={row.id}
+            onClick={() => handleRowClick(row.original)}
+            className="table-row cursor-pointer hover:bg-gray-100"
+          >
+            {row.getVisibleCells().map((cell) => (
+              <td key={cell.id} className="py-3 px-4 border-b">
+                {flexRender(cell.column.columnDef.cell, cell.getContext())}
+              </td>
+            ))}
+            <td className="py-2 px-2 border-b">
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleDelete(row.original);
+                }}
+                className="mr-2 text-red-600"
+              >
+                <DeleteIcon />
+              </button>
+              <button
+                onClick={(e) => {
+                  e.stopPropagation();
+                  handleEdit(row.original);
+                }}
+                className="text-[#6941C6]"
+              >
+                <EditIcon />
+              </button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+export default TableBody;
