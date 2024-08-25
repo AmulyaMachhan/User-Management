eabbe7fd204a3cf5093dd4e81790038caa242e83
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-25 15:33:09 +0530

    Updated the delete modal compoent

--- frontend/src/components/Modals/DeleteModal.jsx
+++ frontend/src/components/Modals/DeleteModal.jsx
@@ -1,6 +1,32 @@
+/* eslint-disable react/prop-types */
 import PropTypes from "prop-types";
+import { useDispatch } from "react-redux";
+import { deleteUser } from "../../redux/userSlice";
 
-const DeleteModal = ({ onClose, onDelete }) => {
+const DeleteModal = ({
+  user,
+  setFilteredData,
+  setModals,
+  filteredData,
+  setSelectedUser,
+  setIsSidePaneOpen,
+}) => {
+  const dispatch = useDispatch();
+
+  const handleConfirmDelete = () => {
+    dispatch(deleteUser(user.id));
+    setFilteredData(filteredData.filter((user) => user.id !== user.id));
+    setModals((prev) => ({ ...prev, isDeleteModalOpen: false }));
+    setSelectedUser(null);
+    setIsSidePaneOpen(false);
+  };
+
+  const handleCloseModal = () => {
+    setModals((prev) => ({ ...prev, isDeleteModalOpen: false }));
+    setSelectedUser(null);
+    setIsSidePaneOpen(true);
+  };
+
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
       <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
@@ -11,13 +37,13 @@ const DeleteModal = ({ onClose, onDelete }) => {
         </p>
         <div className="flex justify-end gap-4">
           <button
-            onClick={onClose}
+            onClick={handleCloseModal}
             className="px-4 py-2 bg-gray-300 rounded-lg hover:bg-gray-400"
           >
             Cancel
           </button>
           <button
-            onClick={onDelete}
+            onClick={handleConfirmDelete}
             className="px-4 py-2 bg-[#6941C6] text-white rounded-lg hover:bg-[#53359e]"
           >
             Delete
