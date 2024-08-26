85d4819a07232930e3198b1610799d19014e8324
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-26 09:20:11 +0530

    Down Arrow Icon

---
+++ frontend/src/components/Icons/DownArrowIcon.jsx
@@ -0,0 +1,21 @@
+function DownArrowIcon() {
+  return (
+    <svg
+      width="12"
+      height="12"
+      viewBox="0 0 12 12"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <path
+        d="M5.99992 1.33337V10.6667M5.99992 10.6667L10.6666 6.00004M5.99992 10.6667L1.33325 6.00004"
+        stroke="#475467"
+        strokeWidth="1.33333"
+        strokeLinecap="round"
+        strokeLinejoin="round"
+      />
+    </svg>
+  );
+}
+
+export default DownArrowIcon;
