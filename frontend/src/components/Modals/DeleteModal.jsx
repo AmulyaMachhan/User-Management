e6312b502346739d57621141adf1c9f9b94ac7c2
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-27 14:12:19 +0530

    Updated the delete modal

--- frontend/src/components/Modals/DeleteModal.jsx
+++ frontend/src/components/Modals/DeleteModal.jsx
@@ -2,6 +2,7 @@
 import PropTypes from "prop-types";
 import { useDispatch } from "react-redux";
 import { deleteUser } from "../../redux/userSlice";
+import { CloseIcon } from "../Icons";
 
 const DeleteModal = ({
   user,
@@ -29,24 +30,23 @@ const DeleteModal = ({
 
   return (
     <div className="fixed inset-0 flex items-center justify-center z-50">
-      <div className="bg-white p-6 rounded-lg shadow-lg max-w-sm w-full">
-        <h2 className="text-xl font-semibold mb-4">Delete Member Details</h2>
-        <p className="mb-6">
+      <div className="bg-white p-6 rounded-lg shadow-lg max-w-lg w-full">
+        <div className="flex justify-between items-center mb-4">
+          <h2 className="text-md font-semibold">Delete Member Details</h2>
+          <button onClick={handleCloseModal}>
+            <CloseIcon color={"#616161"} height="10" width="10" />
+          </button>
+        </div>
+        <p className="mb-6 text-sm">
           Are you sure you want to delete this Member{`'s`} details? This action
           cannot be undone.
         </p>
         <div className="flex justify-end gap-4">
           <button
-            onClick={handleCloseModal}
-            className="px-4 py-2 bg-gray-300 rounded-lg hover:bg-gray-400"
-          >
-            Cancel
-          </button>
-          <button
             onClick={handleConfirmDelete}
-            className="px-4 py-2 bg-[#6941C6] text-white rounded-lg hover:bg-[#53359e]"
+            className="px-4 py-2 bg-[#6941C6] text-white text-sm tracking-wider font-bold rounded-lg hover:bg-[#53359e]"
           >
-            Delete
+            DELETE
           </button>
         </div>
       </div>
