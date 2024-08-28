bd1541899b535c5165cfb2fd537205a8f74645c7
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-28 10:08:19 +0530

    Updated the status column

--- frontend/src/components/Columns/Status.jsx
+++ frontend/src/components/Columns/Status.jsx
@@ -1,13 +1,7 @@
 import PropTypes from "prop-types";
 
 const Status = ({ isActive }) => (
-  <div
-    className={`flex items-center justify-center gap-2 text-xs py-1 px-2 border rounded-xl ${
-      isActive
-        ? "border-green-300 bg-green-50 text-green-600"
-        : "border-red-300 bg-red-50 text-red-600"
-    }`}
-  >
+  <div className="flex items-center justify-center gap-2 text-xs text-[#344054] font-[500] p-1 border border-[#D0D5DD] rounded-lg">
     <span
       className={`h-2 w-2 rounded-full ${
         isActive ? "bg-green-500" : "bg-red-500"
