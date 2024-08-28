507f09c40f0cf518a65b967c757e76f2f521682d
AmulyaMachhan <machhanamulya@gmail.com>
2024-08-28 18:12:11 +0530

    Updated the navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,21 +1,23 @@
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation } from "react-router-dom";
 import { GridIcon } from "./Icons/index";
 
 function Navbar() {
+  const location = useLocation();
+
   return (
     <aside className="w-[200px] flex flex-col">
       <nav className="flex flex-col mt-6 space-y-3 text-sm">
         <NavLink
           to="/"
           className={({ isActive }) =>
-            `flex items-center gap-2 px-5 py-2 text-gray-600 hover:text-[#6941C6] hover:bg-gray-200 ${
-              isActive ? "text-[#6941C6]" : ""
+            `flex items-center gap-2 px-5 py-2  hover:bg-gray-200 ${
+              isActive ? "text-[#6941C6]" : "text-gray-600"
             }`
           }
         >
           {/* Icon */}
           <div>
-            <GridIcon />
+            <GridIcon color={location.pathname === "/" ? "#6941C6" : "black"} />
           </div>
           Overview
         </NavLink>
@@ -23,14 +25,18 @@ function Navbar() {
         <NavLink
           to="/people-directory"
           className={({ isActive }) =>
-            `flex gap-2 items-center px-5 py-2 text-gray-600 hover:text-[#6941C6] hover:bg-gray-200 ${
-              isActive ? "text-[#6941C6]" : ""
+            `flex gap-2 items-center px-5 py-2 hover:bg-gray-200 ${
+              isActive ? "text-[#6941C6]" : "text-gray-600"
             }`
           }
         >
           {/* Icon */}
           <div>
-            <GridIcon />
+            <GridIcon
+              color={
+                location.pathname === "/people-directory" ? "#6941C6" : "black"
+              }
+            />
           </div>
           People Directory
         </NavLink>
